d4d513d64ed7f62cda8dd2212315a51d5044256a
CharanKumarMadipadiga <charanprabhas29@gmail.com>
2024-07-23 14:17:14 +0000

    login route updated

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import LoginRoute from './components/LoginRoute/index'
+
+import './App.css'
+
+const sortByOptions = [
+  {
+    id: 0,
+    displayText: 'Highest',
+    value: 'Highest',
+  },
+  {
+    id: 2,
+    displayText: 'Lowest',
+    value: 'Lowest',
+  },
+]
+
+const App = () => (
+  <>
+    <LoginRoute />
+  </>
+)
+
+export default App
