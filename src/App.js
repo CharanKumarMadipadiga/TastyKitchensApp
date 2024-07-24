263d34ea4e88e0253d6416be065858bbd5ec9bcc
CharanKumarMadipadiga <charanprabhas29@gmail.com>
2024-07-24 07:05:58 +0000

    loginroute responsive updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import LoginRoute from './components/LoginRoute/index'
+import Home from './components/Home/index'
 
 import './App.css'
 
-const sortByOptions = [
-  {
-    id: 0,
-    displayText: 'Highest',
-    value: 'Highest',
-  },
-  {
-    id: 2,
-    displayText: 'Lowest',
-    value: 'Lowest',
-  },
-]
+// const sortByOptions = [
+//   {
+//     id: 0,
+//     displayText: 'Highest',
+//     value: 'Highest',
+//   },
+//   {
+//     id: 2,
+//     displayText: 'Lowest',
+//     value: 'Lowest',
+//   },
+// ]
 
 const App = () => (
-  <>
-    <LoginRoute />
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginRoute} />
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
