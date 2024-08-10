3db75bd8acdaf604a3c14c2f360d12c5a23c93da
CharanKumarMadipadiga <charanprabhas29@gmail.com>
2024-08-10 08:35:49 +0000

    restaurent cards updated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
 import Cookies from 'js-cookie'
-
+import Loader from 'react-loader-spinner'
 import Header from '../Header/index'
 import Carousel from '../Carousel/index'
 import RestaurentItem from '../RestaurentItem/index'
@@ -68,6 +68,23 @@ const Home = () => {
     }
   }
 
+  const loadRestaurentsList = restaurentsCardList =>
+    restaurentsCardList.map(restaurent => (
+      <RestaurentItem key={restaurent.id} restaurentDetails={restaurent} />
+    ))
+
+  const loadSpinner = () => (
+    <div className="carousels-loader-container">
+      <Loader
+        type="TailSpin"
+        color="#F7931E"
+        height="53.33"
+        width="53.33"
+        ariaLabel="tail-spin-loading"
+      />
+    </div>
+  )
+
   useEffect(() => {
     getRestaurentsList()
   }, [])
@@ -105,13 +122,67 @@ const Home = () => {
         </div>
         <hr className="hr-line" />
         <ul className="restrents-list">
-          {restaurentsList.map(restaurent => (
+          {/* {restaurentsList.map(restaurent => (
             <RestaurentItem
               key={restaurent.id}
               restaurentDetails={restaurent}
             />
-          ))}
+          ))} */}
+          {apiStatus === apiConstants.pending
+            ? loadSpinner()
+            : loadRestaurentsList(restaurentsList)}
         </ul>
+        <div className="pagination-container">
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="left-btn"
+          >
+            <g clipPath="url(#clip0_13799_11207)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M17.8735 10L19 11.1507L14.253 16L19 20.8493L17.8735 22L12.6848 16.6995C12.3043 16.3108 12.3043 15.6892 12.6848 15.3005L17.8735 10Z"
+                fill="#334155"
+              />
+            </g>
+            <rect x="0.5" y="0.5" width="31" height="31" stroke="#334155" />
+            <defs>
+              <clipPath id="clip0_13799_11207">
+                <rect width="32" height="32" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <div className="pagination">
+            <p className="pagination-number">1 of 20</p>
+          </div>
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="right-btn"
+          >
+            <g clipPath="url(#clip0_13799_11208)">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M14.1265 22L13 20.8493L17.747 16L13 11.1507L14.1265 10L19.3152 15.3005C19.6957 15.6892 19.6957 16.3108 19.3152 16.6995L14.1265 22Z"
+                fill="#334155"
+              />
+            </g>
+            <rect x="0.5" y="0.5" width="31" height="31" stroke="#334155" />
+            <defs>
+              <clipPath id="clip0_13799_11208">
+                <rect width="32" height="32" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </div>
       </div>
     </>
   )
