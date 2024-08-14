7d31c5ca57f87c4ab46d96dc1d139c03baa9a18c
CharanKumarMadipadiga <charanprabhas29@gmail.com>
2024-08-14 11:43:25 +0000

    food-items-quantity updated

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,18 @@
+import Header from '../Header'
 import './index.css'
 
-const Cart = () => <h1>Cart</h1>
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-con">
+      <div className="cart-header">
+        <span className="cart-headers">Item</span>
+        <span className="cart-headers">Quantity</span>
+        <span className="cart-headers">Price</span>
+      </div>
+      <h1>charan</h1>
+    </div>
+  </>
+)
 
 export default Cart
--- src/components/FoodItemCard/index.js
+++ src/components/FoodItemCard/index.js
@@ -1,3 +1,5 @@
+import {useState} from 'react'
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faIndianRupeeSign} from '@fortawesome/free-solid-svg-icons'
 
@@ -6,6 +8,81 @@ import './index.css'
 const FoodItemCard = props => {
   const {foodCardDetails} = props
   const {id, cost, imageUrl, name, rating} = foodCardDetails
+  const [isAddBtnClicked, setAddBtn] = useState(false)
+  const [activeCount, setActiveCount] = useState(0)
+
+  const onClickAddBtn = () => {
+    setAddBtn(true)
+  }
+
+  const onClickMinus = () => {
+    if (activeCount > 0) {
+      setActiveCount(prevState => prevState - 1)
+    } else {
+      setActiveCount(0)
+    }
+  }
+
+  const onClickPlus = () => {
+    setActiveCount(prevState => prevState + 1)
+  }
+
+  const renderAddQuantityButtons = () => (
+    <div className="quantity-con">
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        cursor="pointer"
+        onClick={onClickMinus}
+      >
+        <rect
+          x="0.375"
+          y="0.375"
+          width="17.25"
+          height="17.25"
+          rx="1.875"
+          stroke="#475569"
+          strokeWidth="0.75"
+        />
+        <rect
+          x="6.00002"
+          y="8.625"
+          width="6"
+          height="0.75"
+          rx="0.375"
+          fill="#3E4C59"
+        />
+      </svg>
+
+      <p className="active-count">{activeCount}</p>
+      <svg
+        width="18"
+        height="18"
+        viewBox="0 0 18 18"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        cursor="pointer"
+        onClick={onClickPlus}
+      >
+        <rect
+          x="0.375"
+          y="0.375"
+          width="17.25"
+          height="17.25"
+          rx="1.875"
+          stroke="#475569"
+          strokeWidth="0.75"
+        />
+        <path
+          d="M11.25 9.375H9.375V11.25C9.375 11.4563 9.20625 11.625 9 11.625C8.79375 11.625 8.625 11.4563 8.625 11.25V9.375H6.75C6.54375 9.375 6.375 9.20625 6.375 9C6.375 8.79375 6.54375 8.625 6.75 8.625H8.625V6.75C8.625 6.54375 8.79375 6.375 9 6.375C9.20625 6.375 9.375 6.54375 9.375 6.75V8.625H11.25C11.4563 8.625 11.625 8.79375 11.625 9C11.625 9.20625 11.4563 9.375 11.25 9.375Z"
+          fill="#475569"
+        />
+      </svg>
+    </div>
+  )
 
   return (
     <li className="food-item-card" key={id} data-testid="foodItem">
@@ -33,28 +110,33 @@ const FoodItemCard = props => {
           </svg>
           <p className="food-item-rating">{rating}</p>
         </div>
-        <svg
-          width="58"
-          height="32"
-          viewBox="0 0 58 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="Add-btn"
-        >
-          <rect
-            x="1"
-            y="1"
-            width="56"
-            height="30"
-            rx="7"
-            stroke="#FFA412"
-            strokeWidth="2"
-          />
-          <path
-            d="M17.696 20L18.4972 17.6903H21.9062L22.7031 20H24.1009L20.9602 11.2727H19.4389L16.2983 20H17.696ZM18.8807 16.5824L20.1676 12.858H20.2358L21.5227 16.5824H18.8807ZM28.1864 20C30.8455 20 32.3839 18.3509 32.3839 15.6236C32.3839 12.9091 30.8455 11.2727 28.2759 11.2727H25.3612V20H28.1864ZM26.6779 18.8494V12.4233H28.195C30.104 12.4233 31.0884 13.5653 31.0884 15.6236C31.0884 17.6903 30.104 18.8494 28.114 18.8494H26.6779ZM36.8466 20C39.5057 20 41.044 18.3509 41.044 15.6236C41.044 12.9091 39.5057 11.2727 36.9361 11.2727H34.0213V20H36.8466ZM35.3381 18.8494V12.4233H36.8551C38.7642 12.4233 39.7486 13.5653 39.7486 15.6236C39.7486 17.6903 38.7642 18.8494 36.7741 18.8494H35.3381Z"
-            fill="#FFA412"
-          />
-        </svg>
+        {isAddBtnClicked ? (
+          renderAddQuantityButtons()
+        ) : (
+          <svg
+            width="58"
+            height="32"
+            viewBox="0 0 58 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="Add-btn"
+            onClick={onClickAddBtn}
+          >
+            <rect
+              x="1"
+              y="1"
+              width="56"
+              height="30"
+              rx="7"
+              stroke="#FFA412"
+              strokeWidth="2"
+            />
+            <path
+              d="M17.696 20L18.4972 17.6903H21.9062L22.7031 20H24.1009L20.9602 11.2727H19.4389L16.2983 20H17.696ZM18.8807 16.5824L20.1676 12.858H20.2358L21.5227 16.5824H18.8807ZM28.1864 20C30.8455 20 32.3839 18.3509 32.3839 15.6236C32.3839 12.9091 30.8455 11.2727 28.2759 11.2727H25.3612V20H28.1864ZM26.6779 18.8494V12.4233H28.195C30.104 12.4233 31.0884 13.5653 31.0884 15.6236C31.0884 17.6903 30.104 18.8494 28.114 18.8494H26.6779ZM36.8466 20C39.5057 20 41.044 18.3509 41.044 15.6236C41.044 12.9091 39.5057 11.2727 36.9361 11.2727H34.0213V20H36.8466ZM35.3381 18.8494V12.4233H36.8551C38.7642 12.4233 39.7486 13.5653 39.7486 15.6236C39.7486 17.6903 38.7642 18.8494 36.7741 18.8494H35.3381Z"
+              fill="#FFA412"
+            />
+          </svg>
+        )}
       </div>
     </li>
   )
