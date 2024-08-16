795b30806b1ad0ada4a16e831ceb79e7ee358423
CharanKumarMadipadiga <charanprabhas29@gmail.com>
2024-08-16 12:29:48 +0000

    toaster component updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Toaster />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
